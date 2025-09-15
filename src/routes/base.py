9e668673a6f17570013b4a2d4f0608623f0d5957
Ahmed Nabil <ahmed.nabil.khairy.10@gmail.com>
2025-09-15 03:25:55 +0300

    Add config.py for env Settings (base.py)

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI ,APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 import os
+from helpers.config import get_settings, Settings
 
 base_router = APIRouter(
     prefix="/api/v1",
@@ -7,8 +8,9 @@ base_router = APIRouter(
 )
 
 @base_router.get("/")
-async def welcome():
-    api_version = os.getenv("APP_VERSION")
-    app_name = os.getenv("APP_NAME")
-    
-    return {"message": f"Welcome to the {app_name} (v{api_version})!"}
+async def welcome(app_settings: Settings = Depends(get_settings)):
+
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
+
+    return {"message": f"Welcome to the {app_name} (v{app_version})!"}
