5f0ac2a7614199d13a398bd50af169e19c92262c
Ahmed Nabil <ahmed.nabil.khairy.10@gmail.com>
2025-09-23 07:55:16 +0300

    Build CoHereProvider

--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -1,10 +1,22 @@
-from enums import Enum
+from enum import Enum
 
-class LLMEnum(Enum):
-    OPENAI = "openai"
-    COHERE = "cohere"
+class LLMEnums(Enum):
+    OPENAI = "OPENAI"
+    COHERE = "COHERE"
 
 class OpenAIEnums(Enum):
     SYSTEM = "system"
     USER = "user"
     ASSISTANT = "assistant"
+
+class CoHereEnums(Enum):
+    SYSTEM = "SYSTEM"
+    USER = "USER"
+    ASSISTANT = "CHATBOT"
+
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
+
+class DocumentTypeEnum(Enum):
+    DOCUMENT = "document"
+    QUERY = "query"
