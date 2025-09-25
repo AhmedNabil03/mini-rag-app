15059d3f3147ea3ef62a6b0a77066be2ce6a24b7
Ahmed Nabil <ahmed.nabil.khairy.10@gmail.com>
2025-09-26 02:43:16 +0300

    Building NLPController & NLP Route

---
+++ src/controllers/NLPController.py
@@ -0,0 +1,86 @@
+from .BaseController import BaseController
+from models.db_schemas import ProjectDBSchema, DataChunkDBSchema
+from typing import List
+from stores.llm.LLMEnums import DocumentTypeEnum
+import json
+
+class NLPController(BaseController):
+    
+    def __init__(self, vectordb_client, generation_client, embedding_client):
+        super().__init__()
+        
+        self.vectordb_client = vectordb_client
+        self.generation_client = generation_client
+        self.embedding_client = embedding_client
+        
+    def create_collection_name(self, project_id: str):
+        return f"collection_{project_id}".strip()
+    
+    def reset_vector_db_collection(self, project: ProjectDBSchema):
+        collection_name = self.create_collection_name(project_id=project.project_id)
+        return self.vectordb_client.delete_collection(collection_name=collection_name)
+    
+    def get_vector_db_collection_info(self, project: ProjectDBSchema):
+        collection_name = self.create_collection_name(project_id=project.project_id)
+        collection_info = self.vectordb_client.get_collection_info(collection_name=collection_name)
+        
+        return json.loads(
+            json.dumps(collection_info, default=lambda x: x.__dict__)
+        )
+
+    def index_into_vector_db(self, project: ProjectDBSchema, chunks: List[DataChunkDBSchema],
+                             chunks_ids = List[int],
+                             do_reset: bool = False):
+        
+        # step 1: get collection name
+        collection_name = self.create_collection_name(project_id=project.project_id)
+        
+        # step 2: manage items
+        texts = [chunk.chunk_text for chunk in chunks]
+        metadatas = [chunk.chunk_metadata for chunk in chunks]
+        vectors = [
+            self.embedding_client.embed_text(text=text, document_type=DocumentTypeEnum.DOCUMENT.value) 
+            for text in texts
+        ]
+        
+        # step 3: create collection if not exists
+        _ = self.vectordb_client.create_collection(
+            collection_name=collection_name, 
+            embedding_size=self.embedding_client.embedding_size, 
+            do_reset=do_reset
+        )
+        
+        # step 4: insert items into collection
+        _ = self.vectordb_client.insert_many(
+            collection_name=collection_name, 
+            texts=texts, 
+            metadata=metadatas, 
+            vectors=vectors,
+            record_ids=chunks_ids
+        )
+        
+        return True
+
+    def search_vector_db_collection(self, project: ProjectDBSchema, text: str, limit: int = 10):
+
+        # step1: get collection name
+        collection_name = self.create_collection_name(project_id=project.project_id)
+
+        # step2: get text embedding vector
+        vector = self.embedding_client.embed_text(text=text, 
+                                                  document_type=DocumentTypeEnum.QUERY.value)
+
+        if not vector or len(vector) == 0:
+            return False
+
+        # step3: do semantic search
+        results = self.vectordb_client.search_by_vector(
+            collection_name=collection_name,
+            vector=vector,
+            limit=limit
+        )
+
+        if not results:
+            return False
+
+        return results
